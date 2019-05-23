9835cea0c26c4131cb49472ccb1b5a9bb9cdb5cc
Warren Amphlett <warren@remarkablelives.co.uk>
2019-05-24 00:31:27 +0100

    Added a doc block to the Anything validator

--- src/Validators/Anything.php
+++ src/Validators/Anything.php
@@ -7,9 +7,16 @@ use SmartDataObjects\Contracts\Validates;
 
 class Anything implements Validates
 {
-	public static function validate($value, $options = []): string
+	/**
+	 * This validation class will just let all data through. This should not be used in production - Instead,
+	 * NULL should be passed as the validator definition for clarity that there is no validation happening.
+	 *
+	 * @param $value
+	 * @param array $options
+	 * @return string
+	 */
+	public static function validate($value, $options = [])
 	{
-		// Literally... Anything!
 		return $value;
 	}
 }
